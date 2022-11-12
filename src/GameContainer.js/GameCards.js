5a25cc27f92b553b3bbe9d117648625fb08f7414
+ <+>
2022-11-12 09:20:28 -0500

    updated gamecards

--- src/GameContainer.js/GameCards.js
+++ src/GameContainer.js/GameCards.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function GameCards({game}) {
   return (
     <div>
       <ol>{game.name}</ol>
+      <Link to={`/games/${game.id}`}><button>Details</button></Link>
     </div>
   )
 }
