b94af624859e8637182145b38fd8c6ae157d1bae
+ <+>
2022-11-14 12:12:37 -0500

    updated home

--- src/Navigation.js/Home.js
+++ src/Navigation.js/Home.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <img id='img' src="https://c4.wallpaperflare.com/wallpaper/481/954/367/playstation-2-collage-video-games-playstation-wallpaper-preview.jpg"></img>
+    </div>
   )
 }
 
