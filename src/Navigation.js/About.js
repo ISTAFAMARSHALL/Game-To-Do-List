7494b0a4239f8110d13daa7291ff0bc6857c73f2
+ <+>
2022-11-14 12:08:19 -0500

    updated about

--- src/Navigation.js/About.js
+++ src/Navigation.js/About.js
@@ -4,7 +4,11 @@ function About() {
 
 
   return (
-    <div>About</div>
+    <div>
+      <p>One of my favorite hobbies is playing videogames.</p>
+      <p>I created this app to track my progession during the playthrough of various games</p>
+      <p>Enjoy using this app to track completion percentage, rate games and leave comments</p>
+    </div>
   )
 }
 
