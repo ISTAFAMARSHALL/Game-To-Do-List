5b552e7810c30c236a138b90161f78fe7f848812
+ <+>
2022-11-20 21:54:47 -0500

    updated genre details

--- src/GenreContainerFolder/GenreDetails.js
+++ src/GenreContainerFolder/GenreDetails.js
@@ -1,3 +1,39 @@
+// import React from 'react'
+// import { useParams } from 'react-router-dom'
+
+// function GenreDetails({genres,games}) {
+
+//   const parId = useParams()
+
+//   const genre = genres.filter((genre) => genre.id === parseInt(parId.id))
+
+//   const genreName = genre.map((g) =>{
+//     return (
+//       <div key={g.id} id='display' >
+//       <h2>{g.name}</h2>
+//       </div>
+//     )
+//     })
+
+//     const filterGames = games.filter((game) => game.genre_id === parseInt(parId.id)) 
+
+//     const displayGames = filterGames.map((g) => {
+//       return (
+//         <li key={g.id}>{g.name}</li>
+//       )
+//     })
+
+//   return (
+//     <div id='display'>
+//       <h2>{genreName}</h2>
+//           {displayGames.length !== 0 ? displayGames : <>Your have not played any games in this genre</> }
+//     </div>
+//   )
+// }
+
+// export default GenreDetails
+
+
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
@@ -7,26 +43,25 @@ function GenreDetails({genres,games}) {
 
   const genre = genres.filter((genre) => genre.id === parseInt(parId.id))
 
-  const genreName = genre.map((g) =>{
-    return (
-      <div key={g.id} id='display' >
-      <h2>{g.name}</h2>
-      </div>
-    )
-    })
+  const DisplayGenreInfo = genre.map((g) =>{
 
-    const filterGames = games.filter((game) => game.genre_id === parseInt(parId.id)) 
-
-    const displayGames = filterGames.map((g) => {
+    const displayGames = g.games.map((g) => {
       return (
         <li key={g.id}>{g.name}</li>
       )
     })
+    
+    return (
+      <div key={g.id} id='display' >
+      <h2>{g.name}</h2>
+      {displayGames.length !== 0 ? displayGames : <>Your have not played any games in this genre</> }
+      </div>
+    )
+    })
 
   return (
     <div id='display'>
-      <h2>{genreName}</h2>
-          {displayGames.length !== 0 ? displayGames : <>Your have not played any games in this genre</> }
+      {DisplayGenreInfo}
     </div>
   )
 }
